d83622a0c5f09bee55c48553c726959cb025709f
steven-cruz <stevencruz.300008@gmail.com>
2021-06-24 15:35:38 -0500

    Basic setup

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+
+const parrafo1 = document.createElement("p");
+parrafo1.innerHTML = "Holberton Dashboard";
+
+const parrafo2 = document.createElement("p");
+parrafo2.innerHTML = "Dashboard data for the students";
+
+const parrafo3 = document.createElement("p");
+parrafo3.innerHTML = "Copyright - Holberton School";
