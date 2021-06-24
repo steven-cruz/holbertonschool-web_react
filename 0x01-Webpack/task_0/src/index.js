79ca0f1e351580296b880513eeedbb2279fbaa9e
steven-cruz <stevencruz.300008@gmail.com>
2021-06-24 15:58:48 -0500

    the insert is changed from js to jquery

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,10 +1,5 @@
 import $ from "jquery";
 
-const parrafo1 = document.createElement("p");
-parrafo1.innerHTML = "Holberton Dashboard";
-
-const parrafo2 = document.createElement("p");
-parrafo2.innerHTML = "Dashboard data for the students";
-
-const parrafo3 = document.createElement("p");
-parrafo3.innerHTML = "Copyright - Holberton School";
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
