b34d7a6d145ff98d6eae28939879997fabf6f9c1
steven-cruz <stevencruz.300008@gmail.com>
2021-06-24 22:20:05 -0500

    lodash is import

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,23 @@
 import $ from "jquery";
 import _ from "lodash";
 
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false,
+  })
+);
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-
-const count = 0;
-
-function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-}
-
-$('button').on('click', _.debounce(updateCounter, 500));
