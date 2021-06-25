21b264e3273f77324dbd79faf222af483fb541d9
steven-cruz <stevencruz.300008@gmail.com>
2021-06-24 22:18:29 -0500

    lodash is import

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
-import $ from "lodash";
+import _ from "lodash";
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
